7268f83b6ed81676b16aa90ff5b42fd3f813056d
Josh Hanni <josh.hanni20@gmail.com>
2022-07-06 13:24:01 -0600

    Git Test

---
+++ gitScript1.sh
@@ -0,0 +1,11 @@
+git init
+
+git add helloworld.py && git commit -m "Initial commit"
+
+git branch -m master main
+
+git branch branch1 && git checkout branch1
+
+git add git_check.py && git commit -m "Committing git_check.py"
+
+git checkout main && git merge branch1
---
+++ gitScript2.sh
@@ -0,0 +1,9 @@
+git clone git@github.com:BYUComputingBootCampTests/git-clone.git
+
+cd git-clone && git log
+
+git diff e4c1d b1cab
+
+git checkout b1cab door.py
+
+git commit -m "Committing previous version"
