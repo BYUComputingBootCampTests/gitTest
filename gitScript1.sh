5aca5245108071d393c6e090c374ffcecc07b3c4
Zephram Tripp <ztripp@byu.edu>
2022-07-13 09:38:51 -0600

    Trying josh's code

--- gitScript1.sh
+++ gitScript1.sh
@@ -1,10 +1,11 @@
 git init
-git add helloworld.py
-git commit -m "msg"
+
+git add helloworld.py && git commit -m "Initial commit"
+
 git branch -m master main
-git branch branch1
-git checkout branch1 # git checkout -b branch1
-git add git_check.py
-git commit -m "msg2"
-git checkout main
-git merge branch1
+
+git branch branch1 && git checkout branch1
+
+git add git_check.py && git commit -m "Commiting git_check.py"
+
+git checkout main && git merge branch1
--- gitScript2.sh
+++ gitScript2.sh
@@ -1,6 +1,9 @@
-git clone https://github.com/BYUComputingBootCampTests/git-clone.git
-cd git-clone
-git log
+git clone git@github.com:BYUComputingBootCampTests/git-clone.git
+
+cd git-clone && git log
+
 git diff e4c1d b1cab
+
 git checkout b1cab door.py
-git commit -m "msg"
+
+git commit -m "Committing previous version"
