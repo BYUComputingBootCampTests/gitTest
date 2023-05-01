8b0844b96591a50af6fdeac263ba00c01ec2de14
Collin Lambert <collinmlambert@gmail.com>
2023-05-01 16:38:52 -0600

    final commit

---
+++ gitScript1.sh
@@ -0,0 +1,10 @@
+git init .
+git branch -m master main
+git add helloworld.py
+git commit -m 'committing helloworld.py'
+git branch branch1
+git checkout branch1
+git add git_check.py
+git commit - m 'committed git_check.py to branch 1'
+git checkout main
+git merge branch 1
---
+++ gitScript2.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/BYUComputingBootCampTests/git-clone.git
+git log
+git diff e4c1d b1cab
+git checkout b1cab door.py
+git commit -m 'modified door.py'
