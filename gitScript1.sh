3ab726fac07ff432ef5a663f845d7a8cedd1a767
Stephen Henstrom <stephen.henstrom@gmail.com>
2023-05-03 11:28:08 -0600

    My test

---
+++ gitScript1.sh
@@ -0,0 +1,10 @@
+ git init
+ git branch -m master main
+ git add helloworld.py
+ git commit -m "adding helloworld.py to main"
+ git branch branch1
+ git switch branch1
+ git add git_check.py
+ git commit -m "adding git_check.py to branch1"
+ git swtich main
+ git merge branch1
---
+++ gitScript2.sh
@@ -0,0 +1,6 @@
+ git clone https://github.com/BYUComputingBootCampTests/git-clone.git
+ git log
+ git checkout e4c1d
+ git checkout b1cab
+ git commit -am "Previous"
+
