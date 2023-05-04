ec7f97fe600ba9a14cf541f52112dbfb3a06d25a
Collin Lambert <collinmlambert@gmail.com>
2023-05-04 09:48:47 -0600

    fixed renaming

--- gitScript1.sh
+++ gitScript1.sh
@@ -1,7 +1,7 @@
 git init .
-git branch -m master main
 git add helloworld.py
 git commit -m 'committing helloworld.py'
+git branch -m master main
 git branch branch1
 git checkout branch1
 git add git_check.py
