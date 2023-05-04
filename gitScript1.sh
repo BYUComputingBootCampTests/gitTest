1c83ee764e4043fe03e9488ca85037233d7a5544
KeenanRileyFaulkner <keenan.r.faulkner@gmail.com>
2023-05-04 09:52:15 -0600

    finished gitTest

--- gitScript1.sh
+++ gitScript1.sh
@@ -1,7 +1,7 @@
 git init
 git branch -m master main
 git add helloworld.py
-git commit -m "added helloworld.py"
+git commit -m "added helloworld.py and renamed master to main"
 git checkout -b branch1 main
 git add git_check.py
 git commit -m "added git_check.py"
