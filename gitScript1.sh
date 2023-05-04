faa89c8524f9eac24d26bcdd2767c3c30c5a67b1
Collin Lambert <collinmlambert@gmail.com>
2023-05-04 09:34:54 -0600

    fixed another typo

--- gitScript1.sh
+++ gitScript1.sh
@@ -5,6 +5,6 @@ git commit -m 'committing helloworld.py'
 git branch branch1
 git checkout branch1
 git add git_check.py
-git commit - m 'committed git_check.py to branch 1'
+git commit -m 'committed git_check.py to branch 1'
 git checkout main
 git merge branch1
